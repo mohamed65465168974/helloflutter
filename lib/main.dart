0a14a0d193253b2cdf31941234ee5609c2a53f00
mohamed65465168974 <mohamedshalaby0097@gmail.com>
2021-10-11 15:06:13 +0200

    ch1 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,32 @@
+import 'package:flutter/widgets.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  // widgets
+  // material app
+  // cupertino app
+  runApp(
+    MaterialApp(
+      home: HomePage(),
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+      ),
+    ),
+  );
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Awesome app'),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      body: Container(
+        child: Text("hi flutter"),
+      ),
     );
   }
 }
-
